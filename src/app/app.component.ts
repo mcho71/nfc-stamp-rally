f05d54d3ba5fc3dce8cc5d75f6550efaf3fd715b
mcho71 <machida.city7110@gmail.com>
2020-12-25 02:25:39 +0900

    NFCをスキャンしてスタンプを押す処理を実装

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,12 @@ const STAMPS = [
   }
 ] as const;
 
+const NFCCardSerialNumberStampNameMap = new Map<string, typeof STAMPS[number]['name']>([
+  ['0', 'head'],
+  ['1', 'neck'],
+  ['2', 'leg']
+]);
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
@@ -27,9 +33,43 @@ export class AppComponent {
       correct: false
     }
   });
+  scanning = false;
+  ndef?: NDEFReader;
 
-  constructor() {
-    this.stamps[0].correct = true;
-    this.stamps[2].correct = true;
+  resetStamp() {
+    for (const stamp of this.stamps) {
+      stamp.correct = false;
+    }
+  }
+
+  async start() {
+    if (!this.scanning && !await this.startScan()) {
+      return;
+    }
+    try {
+      // @ts-ignore
+      this.ndef.addEventListener("reading", ({ serialNumber }) => {
+        const stampName = NFCCardSerialNumberStampNameMap.get(serialNumber);
+        // @ts-ignore
+        const stamp = this.stamps.find(stamp => stamp.name === stampName);
+        if (stamp) {
+          stamp.correct = true;
+        }
+      });
+    } catch (error) {
+      alert(`読み取りエラー: ${error}`);
+    }
+  }
+
+  async startScan() {
+    if (!('NDEFReader' in window)) {
+      alert('Web NFCを有効にしてください。');
+      return false;
+    }
+
+    this.ndef = new NDEFReader();
+    await this.ndef.scan();
+    this.scanning = true;
+    return true;
   }
 }
