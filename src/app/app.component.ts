78a8b9f8573c7602e3434355c2424ae799cdba71
mcho71 <machida.city7110@gmail.com>
2020-12-24 23:34:23 +0900

    見た目を整える

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
 
+const STAMPS = [
+  {
+    name: 'head',
+    imagePath: '/assets/stamps/1.png'
+  },
+  {
+    name: 'neck',
+    imagePath: '/assets/stamps/2.png'
+  },
+  {
+    name: 'leg',
+    imagePath: '/assets/stamps/3.png'
+  }
+] as const;
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styles: []
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'nfc-stamp-rally';
+  stamps = STAMPS.map(stamp => {
+    return {
+      ...stamp,
+      correct: false
+    }
+  });
+
+  constructor() {
+    this.stamps[0].correct = true;
+    this.stamps[2].correct = true;
+  }
 }
